aa44ff93ec438b8609623d3052b6ab78b58e5ba1
justinhuynh <jhuynh@gmail.com>
2016-06-07 09:37:10 -0400

    completed repo

--- db/migrate/20160606202733_create_movies.rb
+++ db/migrate/20160606202733_create_movies.rb
@@ -1,4 +1,10 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :year
+      t.string :synopsis
+      t.integer :rating
+    end
   end
 end
